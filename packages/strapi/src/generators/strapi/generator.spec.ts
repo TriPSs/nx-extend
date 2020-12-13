fcfb55cc816cc93b7652a84a75a47cf554106dfd
Tycho Bokdam <tycho@palmtreecoding.com>
2020-12-13 17:35:16 +0100

    initial commit

---
+++ packages/strapi/src/generators/strapi/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { StrapiGeneratorSchema } from './schema';
+
+describe('strapi generator', () => {
+  let appTree: Tree;
+  const options: StrapiGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
---
+++ packages/strapi/src/generators/strapi/schema.d.ts
@@ -0,0 +1,5 @@
+export interface StrapiGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
