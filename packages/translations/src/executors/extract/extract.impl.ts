c8afea4779ec43dcd4899c3e3e5ea4a18c948c63
Tycho Bokdam <tycho@palmtreecoding.com>
2021-01-28 16:54:41 +0100

    refactor(translations): Some regarding injecting the <projectRoot>

--- packages/translations/src/executors/extract/extract.impl.ts
+++ packages/translations/src/executors/extract/extract.impl.ts
@@ -3,7 +3,9 @@ import extractIntlMessages from 'extract-react-intl-messages'
 import { join } from 'path'
 
 import { ExtractSchema } from './schema'
+import { ExtractSettings } from '../../providers/base.provider'
 import { BaseProvider, getProvider } from '../../providers'
+import { injectProjectRoot } from '../../utils'
 
 export async function runBuilder(
   options: ExtractSchema,
@@ -13,9 +15,10 @@ export async function runBuilder(
   const projectRoot = join(`${context.workspaceRoot}`, `${projectMetadata.root}`)
 
   let provider: BaseProvider = null
-  let settings = {
+  let settings: ExtractSettings = {
     languages: options.languages,
-    defaultLocale: options.defaultLanguage
+    defaultLocale: options.defaultLanguage,
+    outputDirectory: undefined
   }
 
   // If provider is given then try to use it
@@ -24,14 +27,22 @@ export async function runBuilder(
   }
 
   if (provider) {
-    settings = provider.getExtractSettings(projectRoot)
+    settings = provider.getExtractSettings()
+  }
+
+  // If no output directory is defined the use the one from options
+  if (!settings.outputDirectory) {
+    settings.outputDirectory = options.output
   }
 
   try {
+    const sourceDirectory = injectProjectRoot(options.sourceRoot, projectRoot, context.workspaceRoot)
+    const outputDirectory = injectProjectRoot(settings.outputDirectory, projectRoot, context.workspaceRoot)
+
     await extractIntlMessages(
       settings.languages,
-      join(`${context.workspaceRoot}`, options.sourceRoot.replace('<sourceRoot>', `${projectMetadata.sourceRoot}`), options.pattern),
-      join(`${context.workspaceRoot}`, `${projectMetadata.sourceRoot}`, options.output),
+      join(sourceDirectory, options.pattern),
+      outputDirectory,
       {
         defaultLocale: settings.defaultLocale,
         flat: true,
