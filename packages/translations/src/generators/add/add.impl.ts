1c7db003566aa589c0a9a5d2ce407d2a8976b525
Tycho Bokdam <tycho@palmtreecoding.com>
2021-10-14 11:06:47 +0200

    feat(translations): Added `@nx-extend/translations:add` generator

---
+++ packages/translations/src/generators/add/add.impl.ts
@@ -0,0 +1,62 @@
+import { readProjectConfiguration, updateProjectConfiguration, writeJson, offsetFromRoot, Tree } from '@nrwl/devkit'
+import { ProjectConfiguration } from '@nrwl/tao/src/shared/workspace'
+
+import { Schema } from './schema'
+import { join } from 'path'
+
+export default async function (
+  host: Tree,
+  options: Schema
+) {
+  const app = readProjectConfiguration(host, options.target)
+
+  createAppConfigFile(host, app, options.target)
+
+  updateProjectConfiguration(host, options.target, {
+    ...app,
+    targets: {
+      ...app.targets,
+
+      'extract-translations': {
+        executor: '@nx-extend/translations:extract',
+        options: {}
+      },
+      'pull-translations': {
+        executor: '@nx-extend/translations:pull',
+        options: {}
+      },
+      'push-translations': {
+        executor: '@nx-extend/translations:push',
+        options: {}
+      },
+      'translate': {
+        executor: '@nx-extend/translations:translate',
+        options: {}
+      }
+    }
+  })
+}
+
+export function createAppConfigFile(host: Tree, app: ProjectConfiguration, target: string) {
+  const configFileLocation = '.translationsrc.json'
+
+  writeJson(host, join(app.root, configFileLocation), {
+    extends: `${offsetFromRoot(app.root)}${configFileLocation}`,
+    projectName: target
+  })
+
+  // Check if root file exists
+  if (!host.exists(configFileLocation)) {
+    writeJson(host, configFileLocation, {
+      provider: 'none',
+      outputDirectory: '<projectRoot>/src/translations',
+      outputLanguages: '<projectRoot>/src/translations/locales.json',
+      defaultLanguage: 'en',
+      translator: 'none',
+      translatorOptions: {},
+      languages: [
+        'en'
+      ]
+    })
+  }
+}
