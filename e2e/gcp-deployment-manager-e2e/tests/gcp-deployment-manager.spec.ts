fbe392bcc427e221d2a892e34b153d20e3b12423
Tycho Bokdam <tycho@palmtreecoding.com>
2021-10-22 23:27:41 +0200

    test: Removed strapi-e2e and updated other e2e tests

--- e2e/gcp-deployment-manager-e2e/tests/gcp-deployment-manager.spec.ts
+++ e2e/gcp-deployment-manager-e2e/tests/gcp-deployment-manager.spec.ts
@@ -1,58 +1,21 @@
 import {
   checkFilesExist,
   ensureNxProject,
-  readJson,
   runNxCommandAsync,
   uniq
 } from '@nrwl/nx-plugin/testing'
 
 describe('gcp-deployment-manager e2e', () => {
-  it('should create gcp-deployment-manager', async (done) => {
-    const plugin = uniq('gcp-deployment-manager')
-    ensureNxProject(
-      '@nx-extend/gcp-deployment-manager',
-      'dist/packages/gcp-deployment-manager'
-    )
-    await runNxCommandAsync(
-      `generate @nx-extend/gcp-deployment-manager:gcp-deployment-manager ${plugin}`
-    )
 
-    const result = await runNxCommandAsync(`build ${plugin}`)
-    expect(result.stdout).toContain('Executor ran')
+  it('should be able to generate an deployment manager app', async () => {
+    const plugin = uniq('gcp-functions')
+    ensureNxProject('@nx-extend/gcp-deployment-manager', 'dist/packages/gcp-deployment-manager')
 
-    done()
-  })
+    await runNxCommandAsync(`generate @nx-extend/gcp-deployment-manager:init ${plugin}`)
 
-  describe('--directory', () => {
-    it('should create src in the specified directory', async (done) => {
-      const plugin = uniq('gcp-deployment-manager')
-      ensureNxProject(
-        '@nx-extend/gcp-deployment-manager',
-        'dist/packages/gcp-deployment-manager'
-      )
-      await runNxCommandAsync(
-        `generate @nx-extend/gcp-deployment-manager:gcp-deployment-manager ${plugin} --directory subdir`
-      )
-      expect(() =>
-        checkFilesExist(`libs/subdir/${plugin}/src/index.ts`)
-      ).not.toThrow()
-      done()
-    })
-  })
+    expect(() =>
+      checkFilesExist(`apps/${plugin}/src/deployment.yml`)
+    ).not.toThrow()
+  }, 300000)
 
-  describe('--tags', () => {
-    it('should add tags to nx.json', async (done) => {
-      const plugin = uniq('gcp-deployment-manager')
-      ensureNxProject(
-        '@nx-extend/gcp-deployment-manager',
-        'dist/packages/gcp-deployment-manager'
-      )
-      await runNxCommandAsync(
-        `generate @nx-extend/gcp-deployment-manager:gcp-deployment-manager ${plugin} --tags e2etag,e2ePackage`
-      )
-      const nxJson = readJson('nx.json')
-      expect(nxJson.projects[plugin].tags).toEqual(['e2etag', 'e2ePackage'])
-      done()
-    })
-  })
 })
