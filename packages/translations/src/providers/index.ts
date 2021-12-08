86f2e1ba0740c3861b7e2190a5ef4ef3d927b85f
Tycho Bokdam <tycho@palmtreecoding.com>
2021-12-08 22:59:02 +0100

    feat(translations): Feat added SimpleLocalize as provider

--- packages/translations/src/providers/index.ts
+++ packages/translations/src/providers/index.ts
@@ -5,6 +5,7 @@ import BaseProvider from './base.provider'
 import TraduoraProvider, { TraduoraConfig } from './traduora.provider'
 import TransifexProvider, { TransifexConfig } from './transifex.provider'
 import PoeditorProvider, { PoeditorConfig } from './poeditor.provider'
+import SimpleLocalize , { SimpleLocalizeConfig } from './simplelocalize.provider'
 
 export const getProvider = async (provider: string, context: ExecutorContext, configFile: BaseConfigFile): Promise<BaseProvider<any>> => {
   switch (provider) {
@@ -16,6 +17,9 @@ export const getProvider = async (provider: string, context: ExecutorContext, co
 
     case 'poeditor':
       return new PoeditorProvider(context, configFile as PoeditorConfig)
+
+    case 'simplelocalize':
+      return new SimpleLocalize(context, configFile as SimpleLocalizeConfig)
 
     default:
       logger.warn(`"${provider}" is not an valid provider!`)
