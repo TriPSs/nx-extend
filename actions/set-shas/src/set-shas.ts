6eee6ad3e9cc3e9a480d5fcedf4305f1cf1a19a4
Tycho Bokdam <tycho@palmtreecoding.com>
2022-10-28 21:26:59 +0200

    refactor(actions-set-shas): Added some logging

--- actions/set-shas/src/set-shas.ts
+++ actions/set-shas/src/set-shas.ts
@@ -12,11 +12,14 @@ async function run() {
     let baseSha
 
     if (github.context.eventName === 'pull_request') {
+      core.info(`This is a pull request, get sha from "origin/${mainBranchName}"`)
       baseSha = execCommand(`git merge-base origin/${mainBranchName} HEAD`)
 
     } else {
       const tag = execCommand('git describe --tags --abbrev=0', { asString: true })
       baseSha = execCommand(`git rev-parse ${tag}`, { asString: true })
+
+      core.info(`Got base sha from "${tag}"`)
     }
 
     core.setOutput('base', baseSha)
