fa68612540fa9e16eac3a77a9350e7b15e6625b4
Tycho Bokdam <tycho@palmtreecoding.com>
2022-10-28 22:26:20 +0200

    refactor(actions-set-shas): Also export the variables

--- actions/set-shas/dist/index.js
+++ actions/set-shas/dist/index.js
@@ -20415,19 +20415,30 @@ function run() {
         try {
             // Get all options
             const mainBranchName = core.getInput('main-branch-name');
-            const headSha = (0, exec_1.execCommand)('git rev-parse HEAD', { asString: true });
+            const headSha = (0, exec_1.execCommand)('git rev-parse HEAD', {
+                asString: true,
+                silent: !core.isDebug()
+            });
             let baseSha;
             if (github.context.eventName === 'pull_request') {
                 core.info(`This is a pull request, get sha from "origin/${mainBranchName}"`);
                 baseSha = (0, exec_1.execCommand)(`git merge-base origin/${mainBranchName} HEAD`);
             }
             else {
-                const tag = (0, exec_1.execCommand)('git describe --tags --abbrev=0', { asString: true });
-                baseSha = (0, exec_1.execCommand)(`git rev-parse ${tag}`, { asString: true });
+                const tag = (0, exec_1.execCommand)('git describe --tags --abbrev=0', {
+                    asString: true,
+                    silent: !core.isDebug()
+                });
+                baseSha = (0, exec_1.execCommand)(`git rev-parse ${tag}`, {
+                    asString: true,
+                    silent: !core.isDebug()
+                });
                 core.info(`Got base sha from "${tag}"`);
             }
             core.setOutput('base', baseSha);
+            core.exportVariable('NX_BASE', baseSha);
             core.setOutput('head', headSha);
+            core.exportVariable('NX_HEAD', headSha);
         }
         catch (err) {
             core.setFailed(err);
--- actions/set-shas/src/set-shas.ts
+++ actions/set-shas/src/set-shas.ts
@@ -2,13 +2,17 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 import { execCommand } from './utils/exec'
+import { exportVariable } from '@actions/core'
 
 async function run() {
   try {
     // Get all options
     const mainBranchName = core.getInput('main-branch-name')
 
-    const headSha = execCommand('git rev-parse HEAD', { asString: true })
+    const headSha = execCommand('git rev-parse HEAD', {
+      asString: true,
+      silent: !core.isDebug()
+    })
     let baseSha
 
     if (github.context.eventName === 'pull_request') {
@@ -16,14 +20,22 @@ async function run() {
       baseSha = execCommand(`git merge-base origin/${mainBranchName} HEAD`)
 
     } else {
-      const tag = execCommand('git describe --tags --abbrev=0', { asString: true })
-      baseSha = execCommand(`git rev-parse ${tag}`, { asString: true })
+      const tag = execCommand('git describe --tags --abbrev=0', {
+        asString: true,
+        silent: !core.isDebug()
+      })
+      baseSha = execCommand(`git rev-parse ${tag}`, {
+        asString: true,
+        silent: !core.isDebug()
+      })
 
       core.info(`Got base sha from "${tag}"`)
     }
 
     core.setOutput('base', baseSha)
+    core.exportVariable('NX_BASE', baseSha)
     core.setOutput('head', headSha)
+    core.exportVariable('NX_HEAD', headSha)
 
   } catch (err) {
     core.setFailed(err)
