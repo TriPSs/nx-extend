f5086bf2b3cb79ba392fa47679e0bc1a8248eca9
Tycho Bokdam <tycho@palmtreecoding.com>
2022-10-28 21:12:50 +0200

    refactor(actions-set-shas): Change how we run

--- actions/set-shas/src/set-shas.ts
+++ actions/set-shas/src/set-shas.ts
@@ -1,17 +1,17 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 import { execCommand } from './utils/exec'
 
 async function run() {
   try {
     // Get all options
-    const mainBranchName = core.getInput('mainBranchName')
-    const eventName = core.getInput('eventName')
+    const mainBranchName = core.getInput('main-branch-name')
 
     const headSha = execCommand('git rev-parse HEAD', { asString: true })
     let baseSha
 
-    if (eventName === 'pull_request') {
+    if (github.context.eventName === 'pull_request') {
       baseSha = execCommand(`git merge-base origin/${mainBranchName} HEAD`)
 
     } else {
