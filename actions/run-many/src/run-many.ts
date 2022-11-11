2be2b4801f35074ab58238010a9503b5fc766566
Tycho Bokdam <tycho@palmtreecoding.com>
2022-11-11 22:27:57 +0100

    feat(actions-run-many): Also publish as script

--- actions/run-many/src/run-many.ts
+++ actions/run-many/src/run-many.ts
@@ -1,6 +1,17 @@
 import * as core from '@actions/core'
 import { getProjects, logger, workspaceRoot } from '@nrwl/devkit'
 import { FsTree } from 'nx/src/generators/tree'
+import { hideBin } from 'yargs/helpers'
+import yargs from 'yargs/yargs'
+
+const argv = yargs(hideBin(process.argv))
+  .options({
+    tag: { type: 'string' },
+    target: { type: 'string' },
+    parallel: { type: 'number' },
+    includeLibs: { type: 'boolean' }
+  })
+  .parseSync()
 
 import { buildCommand } from './utils/build-command'
 import { execCommand } from './utils/exec'
@@ -12,32 +23,52 @@ async function run() {
     const projects = getProjects(nxTree)
 
     // Get all options
-    const tag = core.getInput('tag')
-    const target = core.getInput('target', { required: true })
+    const tag = core.getInput('tag') || argv.tag
+    const target = core.getInput('target', { required: !argv.target }) || argv.target
     const jobIndex = parseInt(core.getInput('index') || '1', 10)
     const jobCount = parseInt(core.getInput('count') || '1', 10)
-    const parallel = core.getInput('parallel')
+    const parallel = (core.getInput('parallel') || argv.parallel) as string
     const preTargets = core.getMultilineInput('preTargets', { trimWhitespace: true })
     const postTargets = core.getMultilineInput('postTargets', { trimWhitespace: true })
+    // const includeLibs = Boolean(core.getInput('includeLibs')) || argv.includeLibs
 
     core.debug(`Job index ${jobIndex}`)
     core.debug(`Job count ${jobCount}`)
 
-    core.debug(`Pre targets ${JSON.stringify(preTargets)}`)
-    core.debug(`Post targets ${JSON.stringify(postTargets)}`)
+    core.debug(`Pre targets ${JSON.stringify(preTargets, null, 2)}`)
+    core.debug(`Post targets ${JSON.stringify(postTargets, null, 2)}`)
 
     if (tag) {
       core.info(`Running all projects with tag "${tag}"`)
     }
 
+    let affectedProjects = []
     // Get all affected projects
-    const { projects: affectedProjects } = execCommand<{ projects: any }>(buildCommand([
+    const { projects: affectedApps } = execCommand<{ projects: any }>(buildCommand([
       'npx nx print-affected',
-      `--target=${target}`
+      `--target=${target}`,
+      '--type=app'
     ]), {
       asJSON: true,
       silent: !core.isDebug()
     })
+
+    affectedProjects = affectedProjects.concat(affectedApps)
+
+    // if (includeLibs) {
+    //   core.info('Going to include all the libs')
+    //
+    //   const { projects: affectedLibs } = execCommand<{ projects: any }>(buildCommand([
+    //     'npx nx print-affected',
+    //     `--target=${target}`,
+    //     '--type=lib'
+    //   ]), {
+    //     asJSON: true,
+    //     silent: !core.isDebug()
+    //   })
+    //
+    //   affectedProjects = affectedProjects.concat(affectedLibs)
+    // }
 
     // Filter out all projects that are not allowed
     const allowedProjects = Array.from(projects).filter(([project, config]) => {
