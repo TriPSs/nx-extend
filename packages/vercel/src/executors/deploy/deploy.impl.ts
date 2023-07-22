c7814c687b856264c77b4fdd64be0ebe61ec038a
Tycho Bokdam <tycho@palmtreecoding.com>
2023-07-22 15:34:23 +0200

    fix(vercel): Fixed deployment url not being written to summary

--- packages/vercel/src/executors/deploy/deploy.impl.ts
+++ packages/vercel/src/executors/deploy/deploy.impl.ts
@@ -22,18 +22,13 @@ export async function deployExecutor(
   const vercelBuildTarget = Object.keys(targets).find(
     (target) => targets[target].executor === '@nx-extend/vercel:build'
   )
-  const buildTarget =
-    targets[vercelBuildTarget]?.options?.buildTarget || 'build-next'
+  const buildTarget = targets[vercelBuildTarget]?.options?.buildTarget || 'build-next'
 
   if (!targets[buildTarget]?.options?.outputPath) {
     throw new Error(`"${buildTarget}" target has no "outputPath" configured!`)
   }
 
-  if (
-    !existsSync(
-      join(targets[buildTarget].options.outputPath, '.vercel/project.json')
-    )
-  ) {
+  if (!existsSync(join(targets[buildTarget].options.outputPath, '.vercel/project.json'))) {
     throw new Error('No ".vercel/project.json" found in dist folder! ')
   }
 
@@ -63,7 +58,9 @@ export async function deployExecutor(
     )
 
     if (url) {
-      githubCore.summary.addLink('Vercel URL', url.trim())
+      await githubCore.summary
+        .addLink('Vercel URL', url.trim())
+        .write()
     }
   }
 
