b8352d09ded79e0e7905382dceb66908aa0e033e
Tycho Bokdam <tycho@palmtreecoding.com>
2023-11-20 20:55:03 +0100

    refactor(starpi): Remove log

--- packages/strapi/src/executors/build/utils/load-ts-config.ts
+++ packages/strapi/src/executors/build/utils/load-ts-config.ts
@@ -1,5 +1,3 @@
-import { logger } from '@nx/devkit'
-import os from 'os'
 import ts from 'typescript'
 
 export function loadTsConfig(cwd: string, path: string) {
@@ -12,8 +10,6 @@ export function loadTsConfig(cwd: string, path: string) {
   const configFile = ts.readConfigFile(configPath, ts.sys.readFile)
 
   const parsedConfig = ts.parseJsonConfigFileContent(configFile.config, ts.sys, cwd)
-
-  logger.debug(`Loaded user TS config:`, os.EOL, parsedConfig)
 
   return {
     config: parsedConfig,
