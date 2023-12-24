59baa8844b60fa52106958d59b414efa8377d69c
Tycho Bokdam <tycho@palmtreecoding.com>
2023-12-24 17:09:46 +0100

    refactor(e2e): Always log output

--- e2e/utils/run-nx-command-async.ts
+++ e2e/utils/run-nx-command-async.ts
@@ -1,9 +1,17 @@
 import { runNxCommandAsync as _runNxCommandAsync } from '@nx/plugin/testing'
 
-export function runNxCommandAsync(command: string) {
-  return _runNxCommandAsync(command, {
+export async function runNxCommandAsync(command: string): Promise<void> {
+  const { stdout, stderr } = await _runNxCommandAsync(command, {
+    silenceError: true,
     env: {
       YARN_ENABLE_HARDENED_MODE: '0'
     }
   })
+
+  console.log(stdout)
+  if (stderr) {
+    console.error(stderr)
+
+    throw new Error(stderr)
+  }
 }
