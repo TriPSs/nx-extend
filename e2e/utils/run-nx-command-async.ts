fd7c9b4c018afc82fb5ec3dff0bbd2febdb88621
Tycho Bokdam <tycho@palmtreecoding.com>
2023-12-24 16:40:22 +0100

    refactor(e2e): Copy over existing env variables

--- e2e/utils/run-nx-command-async.ts
+++ e2e/utils/run-nx-command-async.ts
@@ -1,12 +1,30 @@
 import { tmpProjPath } from '@nx/plugin/testing'
 import { execSync } from 'child_process'
 
+function getStrippedEnvironmentVariables() {
+  return Object.fromEntries(
+    Object.entries(process.env).filter(([key, value]) => {
+      if (key.startsWith('NX_E2E_')) {
+        return true
+      }
+
+      const allowedKeys = ['NX_PCV3']
+
+      if (key.startsWith('NX_') && !allowedKeys.includes(key)) {
+        return false
+      }
+
+      return ['JEST_WORKER_ID', 'CI'].includes(key)
+    })
+  )
+}
+
 export async function runNxCommandAsync(command: string, options: { env?: object } = {}): Promise<void> {
   execSync(`yarn nx ${command}`, {
     cwd: tmpProjPath(),
     stdio: 'inherit',
     env: {
-      // CI: 'true',
+      ...getStrippedEnvironmentVariables(),
       YARN_ENABLE_HARDENED_MODE: '0',
       ...options.env
     }
