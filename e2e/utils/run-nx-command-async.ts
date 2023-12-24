0b0f342359c748cef081c16634dc8416d962c60d
Tycho Bokdam <tycho@palmtreecoding.com>
2023-12-24 17:34:43 +0100

    refactor(e2e): Misc changes to `runNxCommandAsync`

--- e2e/utils/run-nx-command-async.ts
+++ e2e/utils/run-nx-command-async.ts
@@ -1,18 +1,18 @@
 import { runNxCommandAsync as _runNxCommandAsync } from '@nx/plugin/testing'
 
 export async function runNxCommandAsync(command: string, options: { env?: object } = {}): Promise<void> {
-  const { stdout, stderr } = await _runNxCommandAsync(command, {
+  const { stdout, stderr } = await _runNxCommandAsync(`${command} --verbose`, {
     silenceError: true,
     env: {
+      CI: 'true',
       YARN_ENABLE_HARDENED_MODE: '0',
+      YARN_ENABLE_IMMUTABLE_INSTALLS: 'false',
       ...options.env
     }
   })
 
   console.log(stdout)
   if (stderr) {
-    console.error(stderr)
-
-    throw new Error(stderr)
+    console.log(stderr)
   }
 }
