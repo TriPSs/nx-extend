bc902f78660d712a134b36876fcc70345dc25a87
Tycho Bokdam <tycho@palmtreecoding.com>
2023-12-24 16:35:57 +0100

    refactor(e2e): Allow for passing custom env variables in `runNxCommandAsync`

--- e2e/utils/run-nx-command-async.ts
+++ e2e/utils/run-nx-command-async.ts
@@ -1,32 +1,14 @@
 import { tmpProjPath } from '@nx/plugin/testing'
 import { execSync } from 'child_process'
 
-function getStrippedEnvironmentVariables() {
-  return Object.fromEntries(
-    Object.entries(process.env).filter(([key, value]) => {
-      if (key.startsWith('NX_E2E_')) {
-        return true
-      }
-
-      const allowedKeys = ['NX_PCV3']
-
-      if (key.startsWith('NX_') && !allowedKeys.includes(key)) {
-        return false
-      }
-
-      return key !== 'JEST_WORKER_ID'
-    })
-  )
-}
-
-export async function runNxCommandAsync(command: string): Promise<void> {
+export async function runNxCommandAsync(command: string, options: { env?: object } = {}): Promise<void> {
   execSync(`yarn nx ${command}`, {
     cwd: tmpProjPath(),
     stdio: 'inherit',
     env: {
       // CI: 'true',
       YARN_ENABLE_HARDENED_MODE: '0',
-      ...getStrippedEnvironmentVariables()
+      ...options.env
     }
   })
 }
