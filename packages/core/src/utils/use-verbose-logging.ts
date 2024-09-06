befb4d70ff67ebed9df9131a77f343e35e3d86fd
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2024-09-06 13:57:09 +0200

    fix: space

--- packages/core/src/utils/use-verbose-logging.ts
+++ packages/core/src/utils/use-verbose-logging.ts
@@ -2,4 +2,4 @@ import { isCI } from './is-ci'
 
 export const USE_VERBOSE_LOGGING = isCI() || Boolean(process.env.NX_VERBOSE_LOGGING)
 
-export const USE_VERBOSE_LOGGING_MINIMAL = Boolean(process.env.NX_VERBOSE_LOGGING || process.env. ACTIONS_RUNNER_DEBUG)
+export const USE_VERBOSE_LOGGING_MINIMAL = Boolean(process.env.NX_VERBOSE_LOGGING || process.env.ACTIONS_RUNNER_DEBUG)
