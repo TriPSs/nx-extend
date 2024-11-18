d69630d034ff311a65eefe1b4f151bfd7c01e52e
Tycho Bokdam <tycho@palmtreecoding.com>
2024-11-18 15:56:19 +0100

    refactor(translations): Add JSON file reading to File provider

--- packages/translations/src/providers/file.provider.ts
+++ packages/translations/src/providers/file.provider.ts
@@ -1,10 +1,25 @@
+import { readJsonFile } from '@nx/devkit'
+import { existsSync } from 'fs'
+import { join } from 'path'
+
+import { injectProjectRoot } from '../utils'
 import { BaseConfigFile } from '../utils/config-file'
 import BaseProvider from './base.provider'
 
 export default class File extends BaseProvider<BaseConfigFile> {
 
   public async getTranslations(language: string): Promise<{ [key: string]: string }> {
-    return {}
+    const fileLocation = injectProjectRoot(
+      join(this.config.outputDirectory, `${language}.json`),
+      this.config.projectRoot,
+      this.context.root
+    )
+
+    if (!existsSync(fileLocation)) {
+      return {}
+    }
+
+    return readJsonFile(fileLocation)
   }
 
   public async uploadTranslations(language: string, translations: { [key: string]: string }): Promise<boolean> {
